8a2e4d093566b69e6544b5ad03cf979209d54f93
kshitij-peakdawn <xitizrox@gmail.com>
2026-03-25 11:18:39 +0530

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,19 +1,8 @@
 export default async function handler(req, res) {
-  const apiKey = process.env.NEWS_API_KEY;
-  const oneWeekAgo = new Date();
-  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-  const from = oneWeekAgo.toISOString().split("T")[0];
-
-  const url = new URL("https://newsapi.org/v2/everything");
-  url.searchParams.set("q", "credit card India");
-  url.searchParams.set("from", from);
-  url.searchParams.set("language", "en");
-  url.searchParams.set("sortBy", "relevancy");
-  url.searchParams.set("pageSize", "10");
-  url.searchParams.set("apiKey", apiKey);
-
-  const response = await fetch(url.toString());
-  const data = await response.json();
-
-  return res.status(200).json(data);
+  const response = await fetch(
+    "https://economictimes.indiatimes.com/industry/banking/finance/banking/rssfeeds/13358259.cms",
+    { headers: { "User-Agent": "PixieTimes/1.0" } }
+  );
+  const text = await response.text();
+  return res.status(200).send(text);
 }
